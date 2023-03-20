0a5aa15dc1523c6b4399454a11d32ac51a5ed6c4
bahara <muradi.bahara@yahoo.com>
2023-03-20 11:34:06 +0100

    build rest-api

--- build.gradle.kts
+++ build.gradle.kts
@@ -14,7 +14,10 @@ repositories {
 
 dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-web")
+	implementation("org.springframework.boot:spring-boot-starter-validation")
+	implementation("org.jetbrains:annotations:23.0.0")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
+	testImplementation("org.springframework.boot:spring-boot-starter-webflux")
 }
 
 tasks.withType<Test> {
